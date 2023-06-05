a9412aefd17957fbd99ed649334e81aa85e6cd72
VSushkov <resid.marketing@gmail.com>
2023-06-05 23:55:33 +0300

    first commit

---
+++ src/main/java/ai/adsy/AdsyApplication.java
@@ -0,0 +1,13 @@
+package ai.adsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdsyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ai/adsy/AdsyApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.adsy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
